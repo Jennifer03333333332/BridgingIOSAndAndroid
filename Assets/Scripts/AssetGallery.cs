3f398c21a8801971a9cdf3ac536c413644ce09aa
Jennifer03333333332 <yingxual@andrew.cmu.edu>
2023-04-05 14:19:56 -0400

    asset gallery

--- Assets/Scripts/AssetGallery.cs
+++ Assets/Scripts/AssetGallery.cs
@@ -13,6 +13,11 @@ public class AssetGallery : MonoBehaviour
     [SerializeField]
     public Texture[] m_assetTex;
 
+    [SerializeField]
+    private GameObject txtUI;
+    [SerializeField]
+    public string[] m_assetTxt;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,8 +33,10 @@ public class AssetGallery : MonoBehaviour
     public void OpenAsset(int id)
     {
         eachAssetUI.SetActive(true);
+        //texture
         m_assetImg.texture = m_assetTex[id];
         m_assetImg.SetNativeSize();
-
+        //txt
+        txtUI.GetComponent<Text>().text = m_assetTxt[id];
     }
 }
